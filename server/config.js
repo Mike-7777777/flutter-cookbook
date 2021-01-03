cd5de7098f26f1b64ec1772ae39d8179a8dc5568
JabinGP <1748511181@qq.com>
2021-01-03 19:50:32 +0800

    添加服务器模块, 完成README数据库设计

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    listenAddress:"0.0.0.0",
+    listenPort:3838,
+
+    databasePath:"./sqlite3.db"
+}
---
+++ server/controllers/cookbook.js
@@ -0,0 +1,6 @@
+// url -> cookbook
+
+async function get (ctx){
+    ctx.body = "get cookbook"
+}
+module.exports.get = get
