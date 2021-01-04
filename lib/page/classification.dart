ab85a88b8935a1fecc142bbde475dc3e65015961
Mike-7777777 <mike467901371@yahoo.com>
2021-01-04 20:20:13 +0800

    初始化分类页面

--- lib/page/classification.dart
+++ lib/page/classification.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:my_app/constants.dart';
 import 'package:my_app/values/values.dart';
+import 'package:my_app/widget/cookbook-card.dart';
 
 // 分类页面
 class ClassificationPage extends StatefulWidget {
@@ -22,6 +23,7 @@ class _ClassificationPageState extends State<ClassificationPage> {
           CLASSIFICATION_PAGE_TITLE,
           style: TextStyle(color: AppColors.primaryText),
         ),
+
         // actions: <Widget>[
         //   IconButton(
         //     icon: const Icon(
@@ -35,6 +37,51 @@ class _ClassificationPageState extends State<ClassificationPage> {
         //   ),
         // ]
       ),
+      body: _getPageBody(),
     );
+  }
+
+  Widget _getPageBody() {
+    var listview = ListView(
+      children: getListChildren(),
+    );
+    return listview;
+  }
+
+  List<Widget> getListChildren() {
+    // 组件数组
+    var list = <Widget>[];
+
+    // 添加类别内容
+    list.add(
+      // 固定列数的 GridView
+      Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+        child: GridView.count(
+          childAspectRatio: 81 / 100,
+          // 设置列数
+          crossAxisCount: 2,
+          // 解决 GridView 嵌套在 ListView 中报错问题
+          shrinkWrap: true,
+          // 解决 GridView 嵌套在 ListView 中无法滑动问题
+          physics: new NeverScrollableScrollPhysics(),
+          // 设置内容
+          children: getGridChildren(),
+        ),
+      ),
+    );
+    return list;
+  }
+
+  //如果搜索实现了搜索tag，可以考虑跳转到搜索页面加tag参数，这样可以少做一点页面
+  List<Widget> getGridChildren() {
+    var list = <Widget>[];
+    for (int i = 0; i < 7; i++) {
+      list.add(Padding(
+        padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+        child: CookbookCard(),
+      ));
+    }
+    return list;
   }
 }
