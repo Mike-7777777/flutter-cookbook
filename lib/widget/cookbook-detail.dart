554ce279eeb66e1f5512a54112c5fc88de77dadc
JabinGP <1748511181@qq.com>
2021-01-05 20:45:48 +0800

    merge

--- lib/widget/cookbook-detail.dart
+++ lib/widget/cookbook-detail.dart
@@ -12,7 +12,7 @@ class CookbookDetail extends StatelessWidget {
       'https://www.pinclipart.com/picdir/middle/529-5297054_pork-meat-food-clipart-png.png';
 
   final String defaultBuZhouImage =
-      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20191101%2F17%2F1572601975-NVzXavQCRt.jpg&refer=http%3A%2F%2Fimage.biaobaiju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg';
+      'https://www.pinclipart.com/picdir/middle/529-5297054_pork-meat-food-clipart-png.png';
 
   CookbookDetail(this.id);
   @override
